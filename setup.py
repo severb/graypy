51a947aeeb37710ee2417f55b2fe293d2714ff15
severb <none@none>
2011-02-26 23:56:30 +0200

    Initial.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='graypy',
+    version='0.1',
+    description="Python logging handler that sends messages in GELF (Graylog Extended Log Format).",
+    long_description="",
+    keywords='logging gelf graylog2 graylog udp',
+    author='Sever Banesiu',
+    author_email='banesiu.sever@gmail.com',
+    url='http://bitbucket.org/severb/graypy',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+
+)
