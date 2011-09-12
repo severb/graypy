4e98b86a5c863dea3855bcd665757b9ef8ae6446
Sever Banesiu <banesiu.sever@gmail.com>
2011-09-12 21:30:24 +0300

    Made README github compatible.

--- README.rst
+++ README.rst
@@ -1,5 +1,3 @@
-.. -*- restructuredtext -*-
-
 Installing
 ==========
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='graypy',
     version='0.2.1',
     description="Python logging handler that sends messages in GELF (Graylog Extended Log Format).",
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
     keywords='logging gelf graylog2 graylog udp',
     author='Sever Banesiu',
     author_email='banesiu.sever@gmail.com',
