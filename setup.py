77d864b76de20350a4bc883f2339d5d2c88ca683
Sever Banesiu <banesiu.sever@gmail.com>
2011-09-12 21:19:52 +0300

    Bumbed version and changed the url.

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 setup(
 
     name='graypy',
-    version='0.1',
+    version='0.2.1',
     description="Python logging handler that sends messages in GELF (Graylog Extended Log Format).",
     long_description=open('README').read(),
     keywords='logging gelf graylog2 graylog udp',
     author='Sever Banesiu',
     author_email='banesiu.sever@gmail.com',
-    url='http://bitbucket.org/severb/graypy',
+    url='https://github.com/severb/graypy',
     license='BSD',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
 
