6bcd88061d893238ac884d6897ad4a55c708a419
SjonHortensius <sjon@hortensius.net>
2013-06-03 18:09:30 +0300

    Prefix import with 'graypy.'
    needed for python3

--- graypy/__init__.py
+++ graypy/__init__.py
@@ -1,4 +1,4 @@
-from handler import GELFHandler, WAN_CHUNK, LAN_CHUNK
+from graypy.handler import GELFHandler, WAN_CHUNK, LAN_CHUNK
 try:
     from rabbitmq import GELFRabbitHandler, ExcludeFilter
 except ImportError:
