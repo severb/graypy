0838d7dfa48b784a519c1dbc65dd3296c898efc4
Mark Janssen <mark@praseodym.net>
2016-10-12 21:21:03 +0200

    Support for publishing messages with a routing key

--- graypy/rabbitmq.py
+++ graypy/rabbitmq.py
@@ -35,8 +35,8 @@ class GELFRabbitHandler(SocketHandler):
     """
 
     def __init__(self, url, exchange='logging.gelf', debugging_fields=True,
-            extra_fields=True, fqdn=False, exchange_type='fanout', localname=None,
-            facility=None, virtual_host='/'):
+                 extra_fields=True, fqdn=False, exchange_type='fanout', localname=None,
+                 facility=None, virtual_host='/', routing_key=''):
         self.url = url
         parsed = urlparse(url)
         if parsed.scheme != 'amqp':
@@ -59,12 +59,13 @@ class GELFRabbitHandler(SocketHandler):
         self.localname = localname
         self.facility = facility
         self.virtual_host = virtual_host
+        self.routing_key = routing_key
         SocketHandler.__init__(self, host, port)
         self.addFilter(ExcludeFilter('amqplib'))
 
     def makeSocket(self, timeout=1):
         return RabbitSocket(self.cn_args, timeout, self.exchange,
-            self.exchange_type)
+                            self.exchange_type, self.routing_key)
 
     def makePickle(self, record):
         message_dict = make_message_dict(
@@ -74,12 +75,12 @@ class GELFRabbitHandler(SocketHandler):
 
 
 class RabbitSocket(object):
-
-    def __init__(self, cn_args, timeout, exchange, exchange_type):
+    def __init__(self, cn_args, timeout, exchange, exchange_type, routing_key):
         self.cn_args = cn_args
         self.timeout = timeout
         self.exchange = exchange
         self.exchange_type = exchange_type
+        self.routing_key = routing_key
         self.connection = amqp.Connection(
             connection_timeout=timeout, **self.cn_args)
         self.channel = self.connection.channel()
@@ -92,7 +93,7 @@ class RabbitSocket(object):
 
     def sendall(self, data):
         msg = amqp.Message(data, delivery_mode=2)
-        self.channel.basic_publish(msg, exchange=self.exchange)
+        self.channel.basic_publish(msg, exchange=self.exchange, routing_key=self.routing_key)
 
     def close(self):
         try:
@@ -102,7 +103,6 @@ class RabbitSocket(object):
 
 
 class ExcludeFilter(Filter):
-
     def __init__(self, name):
         """Initialize filter.
 
