2dd28282d739ebfa8524f48d1ea3b542aa63ebc2
nklapste <nklapste@ualberta.ca>
2018-09-26 09:48:10 -0600

    adding pylintrc

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ VERSION = find_version("graypy", "__init__.py")
 class Pylint(test):
     def run_tests(self):
         from pylint.lint import Run
-        Run(["graypy", "--persistent", "y"])
+        Run(["graypy", "--persistent", "y", "--rcfile", ".pylintrc"])
 
 
 class PyTest(test):
