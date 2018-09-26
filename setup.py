64b2ed7b773709494b1cc0985f81402936806690
Nathan Klapstein <nklapste@ualberta.ca>
2018-09-25 23:14:00 -0600

    defining version in `__init__`

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""setup.py for graypy"""
+
+import codecs
+import os
+import re
 
 from setuptools import setup, find_packages
 
-setup(
 
+def find_version(*file_paths):
+    with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *file_paths), 'r') as fp:
+        version_file = fp.read()
+    m = re.search(r"^__version__ = \((\d+), ?(\d+), ?(\d+)\)", version_file, re.M)
+    if m:
+        return "{}.{}.{}".format(*m.groups())
+    raise RuntimeError("Unable to find a valid version")
+
+
+# version is defined by a tuple value under __version__
+VERSION = find_version("graypy", "__init__.py")
+
+
+setup(
     name='graypy',
-    version='0.3.1',
+    version=VERSION,
     description="Python logging handler that sends messages in GELF (Graylog Extended Log Format).",
     long_description=open('README.rst').read(),
     keywords='logging gelf graylog2 graylog udp amqp',
@@ -17,7 +37,9 @@ setup(
     include_package_data=True,
     zip_safe=False,
     extras_require={'amqp': ['amqplib==1.0.2']},
-    classifiers=['License :: OSI Approved :: BSD License',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 3'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ],
 )
