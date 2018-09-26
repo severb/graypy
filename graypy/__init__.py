97189863c34cb4b8f02f19e584c6df60b40bbc21
nklapste <nklapste@ualberta.ca>
2018-09-26 08:38:00 -0600

    Adding testing hooks in `setup.py`
    moving version info into `__init__.py`

--- graypy/__init__.py
+++ graypy/__init__.py
@@ -1,5 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""graypy
+
+Python logging handler that sends messages in
+GELF (Graylog Extended Log Format).
+"""
+
 from graypy.handler import GELFHandler, GELFTcpHandler, WAN_CHUNK, LAN_CHUNK
 try:
     from graypy.rabbitmq import GELFRabbitHandler, ExcludeFilter
 except ImportError:
-    pass # amqplib is probably not installed
+    pass  # amqplib is probably not installed
+
+
+__version__ = (0, 3, 1)
