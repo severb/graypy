c05bb893b8f1ff1c05d1c4e747a130c03612e637
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-15 01:41:26 -0700

    adding more `classifiers` in `setup`

--- setup.py
+++ setup.py
@@ -69,8 +69,19 @@ setup(
     extras_require={'amqp': ['amqplib==1.0.2']},
     classifiers=[
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: System :: Logging',
     ],
     cmdclass={"test": PyTest, "lint": Pylint},
 )
