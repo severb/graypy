9fd4d5eb9d4006b2c62b3892636c6a94ed0727a4
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-17 15:27:32 -0700

    adding another test for status field issue

--- tests/integration/test_status_field_issue.py
+++ tests/integration/test_status_field_issue.py
@@ -12,17 +12,28 @@ from tests.helper import get_graylog_response, get_unique_message, \
                     reason="local graylog instance not up")
 def test_non_status_field_log(logger):
     message = get_unique_message()
-    logger.error(message, extra={'foo': 'bar'})
+    logger.error(message, extra={"foo": "bar"})
     graylog_response = get_graylog_response(message, fields=["foo"])
-    assert message == graylog_response['message']
-    assert "bar" == graylog_response['_foo']
+    assert message == graylog_response["message"]
+    assert "bar" == graylog_response["foo"]
 
 
 @pytest.mark.skipif(not LOCAL_GRAYLOG_UP,
                     reason="local graylog instance not up")
 def test_status_field_issue(logger):
     message = get_unique_message()
-    logger.error(message, extra={'status': 'OK'})
+    logger.error(message, extra={"status": "OK"})
     graylog_response = get_graylog_response(message, fields=["status"])
-    assert message == graylog_response['message']
-    assert "OK" == graylog_response['status']
+    assert message == graylog_response["message"]
+    assert "OK" == graylog_response["status"]
+
+
+@pytest.mark.skipif(not LOCAL_GRAYLOG_UP,
+                    reason="local graylog instance not up")
+def test_status_field_issue_multi(logger):
+    message = get_unique_message()
+    logger.error(message, extra={"foo": "bar", "status": "OK"})
+    graylog_response = get_graylog_response(message, fields=["foo", "status"])
+    assert message == graylog_response["message"]
+    assert "bar" == graylog_response["foo"]
+    assert "OK" == graylog_response["status"]
