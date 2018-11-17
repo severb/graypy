fc577d936bca42f4f49236a66db0b243b3ccee4b
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-17 15:04:48 -0700

    refactoring

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -8,12 +8,16 @@
     These tests require an local instance of graylog to send messages to.
 """
 
-
 import requests
 
 
-try:
-    requests.get("http://127.0.0.1:9000/api")
-    LOCAL_GRAYLOG_UP = True
-except Exception:
-    LOCAL_GRAYLOG_UP = False
+def validate_local_graylog_up():
+    """Test to see if a localhost instance of graylog is currently running"""
+    try:
+        requests.get("http://127.0.0.1:9000/api")
+        return True
+    except Exception:
+        return False
+
+
+LOCAL_GRAYLOG_UP = validate_local_graylog_up()
