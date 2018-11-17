6ca1746ad0f67af36808d435e7353d82c41e3f21
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-17 16:31:15 -0700

    flipping assert

--- tests/integration/helper.py
+++ tests/integration/helper.py
@@ -43,5 +43,5 @@ def _get_api_response(message, fields):
 def _parse_api_response(api_response):
     assert api_response.status_code == 200
     messages = api_response.json()["messages"]
-    assert len(messages) == 1
+    assert 1 == len(messages)
     return messages[0]["message"]
