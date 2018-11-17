541cec73b42d464c29c5b1f3c9f11b02492f13e0
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-17 15:13:59 -0700

    attempting fix on integration tests

--- tests/integration/test_status_field_issue.py
+++ tests/integration/test_status_field_issue.py
@@ -13,7 +13,7 @@ from tests.helper import get_graylog_response, get_unique_message, \
 def test_non_status_field_log(logger):
     message = get_unique_message()
     logger.error(message, extra={'foo': 'bar'})
-    graylog_response = get_graylog_response(message)
+    graylog_response = get_graylog_response(message, fields=["foo"])
     assert message == graylog_response['message']
     assert "bar" == graylog_response['_foo']
 
@@ -23,6 +23,6 @@ def test_non_status_field_log(logger):
 def test_status_field_issue(logger):
     message = get_unique_message()
     logger.error(message, extra={'status': 'OK'})
-    graylog_response = get_graylog_response(message)
+    graylog_response = get_graylog_response(message, fields=["status"])
     assert message == graylog_response['message']
     assert "OK" == graylog_response['status']
