8cc2f2446dfd576ae08ae7f2b5f8133ae37db341
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-17 22:35:14 -0700

    attempting to parameterize integration tests

--- tests/integration/test_common_logging.py
+++ tests/integration/test_common_logging.py
@@ -7,20 +7,17 @@ import logging
 
 import pytest
 
-from graypy.handler import SYSLOG_LEVELS, GELFUDPHandler
+from graypy.handler import SYSLOG_LEVELS
 
 from tests.integration import LOCAL_GRAYLOG_UP
-from tests.helper import TEST_UDP_PORT
+from tests.helper import handler, logger
 from tests.integration.helper import get_unique_message, get_graylog_response
 
 
 @pytest.mark.skipif(not LOCAL_GRAYLOG_UP,
                     reason="local graylog instance not up")
-def test_chunked_logging():
+def test_chunked_logging(logger):
     """Test sending a common usage log"""
-    logger = logging.getLogger("test_common_logger")
-    handler = GELFUDPHandler("127.0.0.1", TEST_UDP_PORT)
-    logger.addHandler(handler)
     message = get_unique_message()
     logger.error(message)
     graylog_response = get_graylog_response(message)
