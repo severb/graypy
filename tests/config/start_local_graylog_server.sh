1f97dc84f7e4a00a90d08d926b93974391b9a352
Nathan Klapstein <nklapste@ualberta.ca>
2019-09-18 18:38:20 -0400

    fixing args to rabbitmqadmin

--- tests/config/start_local_graylog_server.sh
+++ tests/config/start_local_graylog_server.sh
@@ -19,9 +19,9 @@ sleep 40
 
 docker-compose exec rabbitmq rabbitmqctl add_user rabbitmq rabbitmq
 docker-compose exec rabbitmq rabbitmqctl set_permissions -p / rabbitmq ".*" ".*" ".*"
-docker-compose exec rabbitmq rabbitmqadmin declare exchange name=logging.gelf type=direct durable=true auto-delete=false internal=false
-docker-compose exec rabbitmq rrabbitmqctl stop_app
-docker-compose exec rabbitmq rrabbitmqctl start_app
+docker-compose exec rabbitmq rabbitmqadmin declare exchange name=logging.gelf type=direct
+docker-compose exec rabbitmq rabbitmqctl stop_app
+docker-compose exec rabbitmq rabbitmqctl start_app
 
 # test that the graylog server docker container is started
 curl -u admin:admin 'http://127.0.0.1:9000/api/search/universal/relative?query=test&range=5&fields=message' || true
