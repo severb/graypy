62cff0e21e4ee030c40580f00c1c349291172382
Nathan Klapstein <nklapste@ualberta.ca>
2019-09-18 20:08:23 -0400

    adding a curl statement for testing status of rabbitmq

--- tests/config/start_local_graylog_server.sh
+++ tests/config/start_local_graylog_server.sh
@@ -19,3 +19,6 @@ sleep 40
 
 # test that the graylog server docker container is started
 curl -u admin:admin 'http://127.0.0.1:9000/api/search/universal/relative?query=test&range=5&fields=message' || true
+
+# test that rabbitmq docker container is started
+ curl --user rabbitmq:rabbitmq 'http://127.0.0.1:15672/api/vhosts'
