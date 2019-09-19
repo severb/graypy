7abfd0789fdcedf8d522f07c1d953064e1d1daef
Nathan Klapstein <nklapste@ualberta.ca>
2019-09-18 20:52:25 -0400

    more fixups

--- tests/config/start_local_graylog_server.sh
+++ tests/config/start_local_graylog_server.sh
@@ -14,13 +14,13 @@ bash create_ssl_certs.sh -h localhost -i 127.0.0.1
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml up -d
 
+docker-compose exec rabbitmq rabbitmqctl stop_app
+docker-compose exec rabbitmq rabbitmqctl start_app
+
 # wait for the graylog server docker container to start
 sleep 40
 
-docker-compose exec rabbitmq rabbitmqctl stop_app
-docker-compose exec rabbitmq rabbitmqctl start_app
 # wait for the graylog server docker container to start
-sleep 40
 
 # test that the graylog server docker container is started
 curl -u admin:admin 'http://127.0.0.1:9000/api/search/universal/relative?query=test&range=5&fields=message' || true
@@ -28,3 +28,4 @@ curl -u admin:admin 'http://127.0.0.1:9000/api/search/universal/relative?query=t
 # test that rabbitmq docker container is started
  curl --user rabbitmq:rabbitmq 'http://127.0.0.1:15672/api/vhosts'
  curl --user rabbitmq:rabbitmq 'http://localhost:15672/api/vhosts'
+ curl --user rabbitmq:rabbitmq 'http://rabbitmq:15672/api/vhosts'
