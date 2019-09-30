868d09c26a7e243da0a2d4bc537d0f29f032660a
Nathan Klapstein <nklapste@ualberta.ca>
2019-09-30 18:01:18 -0400

    Feature/black format (#120)
    * using `black` formatting
    
    * adding `pre-commit` hooks for black

--- tests/helper.py
+++ tests/helper.py
@@ -9,8 +9,7 @@ These functions are used for both the integration and unit testing.
 import logging
 import pytest
 
-from graypy import GELFUDPHandler, GELFTCPHandler, GELFTLSHandler, \
-    GELFHTTPHandler
+from graypy import GELFUDPHandler, GELFTCPHandler, GELFTLSHandler, GELFHTTPHandler
 
 TEST_CERT = "tests/config/localhost.cert.pem"
 KEY_PASS = "secret"
@@ -21,23 +20,45 @@ TEST_HTTP_PORT = 12203
 TEST_TLS_PORT = 12204
 
 
-@pytest.fixture(params=[
-    GELFTCPHandler("127.0.0.1", TEST_TCP_PORT),
-    GELFTCPHandler("127.0.0.1", TEST_TCP_PORT, extra_fields=True),
-    GELFTCPHandler("127.0.0.1", TEST_TCP_PORT, extra_fields=True, debugging_fields=True),
-    GELFTLSHandler("localhost", TEST_TLS_PORT),
-    GELFTLSHandler("localhost", TEST_TLS_PORT, validate=True, ca_certs=TEST_CERT),
-    GELFTLSHandler("127.0.0.1", TEST_TLS_PORT),
-    GELFTLSHandler("127.0.0.1", TEST_TLS_PORT, validate=True, ca_certs=TEST_CERT),
-    GELFHTTPHandler('127.0.0.1', TEST_HTTP_PORT),
-    GELFHTTPHandler('127.0.0.1', TEST_HTTP_PORT, compress=False),
-    GELFUDPHandler("127.0.0.1", TEST_UDP_PORT),
-    GELFUDPHandler("127.0.0.1", TEST_UDP_PORT, compress=False),
-    # the below handler tests are essentially smoke tests
-    # that help cover the argument permutations of BaseGELFHandler
-    GELFUDPHandler("127.0.0.1", TEST_UDP_PORT, debugging_fields=True, extra_fields=True, localname="foobar_localname", facility="foobar_facility", level_names=True, compress=False),
-    GELFUDPHandler("127.0.0.1", TEST_UDP_PORT, debugging_fields=True, extra_fields=True, fqdn=True, facility="foobar_facility", level_names=True, compress=False),
-])
+@pytest.fixture(
+    params=[
+        GELFTCPHandler("127.0.0.1", TEST_TCP_PORT),
+        GELFTCPHandler("127.0.0.1", TEST_TCP_PORT, extra_fields=True),
+        GELFTCPHandler(
+            "127.0.0.1", TEST_TCP_PORT, extra_fields=True, debugging_fields=True
+        ),
+        GELFTLSHandler("localhost", TEST_TLS_PORT),
+        GELFTLSHandler("localhost", TEST_TLS_PORT, validate=True, ca_certs=TEST_CERT),
+        GELFTLSHandler("127.0.0.1", TEST_TLS_PORT),
+        GELFTLSHandler("127.0.0.1", TEST_TLS_PORT, validate=True, ca_certs=TEST_CERT),
+        GELFHTTPHandler("127.0.0.1", TEST_HTTP_PORT),
+        GELFHTTPHandler("127.0.0.1", TEST_HTTP_PORT, compress=False),
+        GELFUDPHandler("127.0.0.1", TEST_UDP_PORT),
+        GELFUDPHandler("127.0.0.1", TEST_UDP_PORT, compress=False),
+        # the below handler tests are essentially smoke tests
+        # that help cover the argument permutations of BaseGELFHandler
+        GELFUDPHandler(
+            "127.0.0.1",
+            TEST_UDP_PORT,
+            debugging_fields=True,
+            extra_fields=True,
+            localname="foobar_localname",
+            facility="foobar_facility",
+            level_names=True,
+            compress=False,
+        ),
+        GELFUDPHandler(
+            "127.0.0.1",
+            TEST_UDP_PORT,
+            debugging_fields=True,
+            extra_fields=True,
+            fqdn=True,
+            facility="foobar_facility",
+            level_names=True,
+            compress=False,
+        ),
+    ]
+)
 def handler(request):
     return request.param
 
--- tests/unit/test_chunking.py
+++ tests/unit/test_chunking.py
@@ -10,28 +10,29 @@ import zlib
 
 import pytest
 
-from graypy.handler import GELFTruncatingChunker, GELFWarningChunker, \
-    BaseGELFChunker, BaseGELFHandler, SYSLOG_LEVELS, GELFChunkOverflowWarning, \
-    GELFTruncationFailureWarning
+from graypy.handler import (
+    GELFTruncatingChunker,
+    GELFWarningChunker,
+    BaseGELFChunker,
+    BaseGELFHandler,
+    SYSLOG_LEVELS,
+    GELFChunkOverflowWarning,
+    GELFTruncationFailureWarning,
+)
 
 
 @pytest.mark.parametrize(
-    "gelf_chunker",
-    [
-        BaseGELFChunker,
-        GELFWarningChunker,
-        GELFTruncatingChunker
-    ]
+    "gelf_chunker", [BaseGELFChunker, GELFWarningChunker, GELFTruncatingChunker]
 )
 def test_gelf_chunking(gelf_chunker):
     """Test various GELF chunkers"""
-    message = b'12345'
-    header = b'\x1e\x0f'
+    message = b"12345"
+    header = b"\x1e\x0f"
     chunks = list(gelf_chunker(chunk_size=2).chunk_message(message))
     expected = [
-        (struct.pack('b', 0), struct.pack('b', 3), b'12'),
-        (struct.pack('b', 1), struct.pack('b', 3), b'34'),
-        (struct.pack('b', 2), struct.pack('b', 3), b'5')
+        (struct.pack("b", 0), struct.pack("b", 3), b"12"),
+        (struct.pack("b", 1), struct.pack("b", 3), b"34"),
+        (struct.pack("b", 2), struct.pack("b", 3), b"5"),
     ]
 
     assert len(chunks) == len(expected)
@@ -49,50 +50,60 @@ def rebuild_gelf_bytes_from_udp_chunks(chunks):
     bsize = len(chunks[0])
     for chunk in chunks:
         if len(chunk) < bsize:
-            gelf_bytes += chunk[-(bsize-len(chunk)):]
+            gelf_bytes += chunk[-(bsize - len(chunk)) :]
         else:
-            gelf_bytes += chunk[((2+struct.calcsize("QBB"))-len(chunk)):]
+            gelf_bytes += chunk[((2 + struct.calcsize("QBB")) - len(chunk)) :]
     return gelf_bytes
 
 
 @pytest.mark.parametrize(
-    "gelf_chunker",
-    [
-        BaseGELFChunker,
-        GELFWarningChunker,
-        GELFTruncatingChunker
-    ]
+    "gelf_chunker", [BaseGELFChunker, GELFWarningChunker, GELFTruncatingChunker]
 )
 def test_gelf_chunkers(gelf_chunker):
     message = BaseGELFHandler().makePickle(
-        logging.LogRecord("test_gelf_chunkers", logging.INFO,
-                          None, None, "1" * 10, None, None))
+        logging.LogRecord(
+            "test_gelf_chunkers", logging.INFO, None, None, "1" * 10, None, None
+        )
+    )
     chunks = list(gelf_chunker(chunk_size=2).chunk_message(message))
     assert len(chunks) <= 128
 
 
 @pytest.mark.parametrize(
-    "gelf_chunker",
-    [
-        BaseGELFChunker,
-        GELFWarningChunker,
-        GELFTruncatingChunker
-    ]
+    "gelf_chunker", [BaseGELFChunker, GELFWarningChunker, GELFTruncatingChunker]
 )
 def test_gelf_chunkers_overflow(gelf_chunker):
     message = BaseGELFHandler().makePickle(
-        logging.LogRecord("test_gelf_chunkers_overflow", logging.INFO,
-                          None, None, "1" * 1000, None, None))
+        logging.LogRecord(
+            "test_gelf_chunkers_overflow",
+            logging.INFO,
+            None,
+            None,
+            "1" * 1000,
+            None,
+            None,
+        )
+    )
     chunks = list(gelf_chunker(chunk_size=1).chunk_message(message))
     assert len(chunks) <= 128
 
 
 def test_chunk_overflow_truncate_uncompressed():
     message = BaseGELFHandler(compress=False).makePickle(
-        logging.LogRecord("test_chunk_overflow_truncate_uncompressed",
-                          logging.INFO, None, None, "1" * 1000, None, None))
+        logging.LogRecord(
+            "test_chunk_overflow_truncate_uncompressed",
+            logging.INFO,
+            None,
+            None,
+            "1" * 1000,
+            None,
+            None,
+        )
+    )
     with pytest.warns(GELFChunkOverflowWarning):
-        chunks = list(GELFTruncatingChunker(chunk_size=2, compress=False).chunk_message(message))
+        chunks = list(
+            GELFTruncatingChunker(chunk_size=2, compress=False).chunk_message(message)
+        )
     assert len(chunks) <= 128
     payload = rebuild_gelf_bytes_from_udp_chunks(chunks).decode("UTF-8")
     glef_json = json.loads(payload)
@@ -103,12 +114,24 @@ def test_chunk_overflow_truncate_uncompressed():
 
 def test_chunk_overflow_truncate_compressed():
     message = BaseGELFHandler(compress=True).makePickle(
-        logging.LogRecord("test_chunk_overflow_truncate_compressed",
-                          logging.INFO, None, None, "123412345" * 5000, None, None))
+        logging.LogRecord(
+            "test_chunk_overflow_truncate_compressed",
+            logging.INFO,
+            None,
+            None,
+            "123412345" * 5000,
+            None,
+            None,
+        )
+    )
     with pytest.warns(GELFChunkOverflowWarning):
-        chunks = list(GELFTruncatingChunker(chunk_size=2, compress=True).chunk_message(message))
+        chunks = list(
+            GELFTruncatingChunker(chunk_size=2, compress=True).chunk_message(message)
+        )
     assert len(chunks) <= 128
-    payload = zlib.decompress(rebuild_gelf_bytes_from_udp_chunks(chunks)).decode("UTF-8")
+    payload = zlib.decompress(rebuild_gelf_bytes_from_udp_chunks(chunks)).decode(
+        "UTF-8"
+    )
     glef_json = json.loads(payload)
     assert glef_json["_chunk_overflow"] is True
     assert glef_json["short_message"] in "123412345" * 5000
@@ -117,14 +140,33 @@ def test_chunk_overflow_truncate_compressed():
 
 def test_chunk_overflow_truncate_fail():
     message = BaseGELFHandler().makePickle(
-        logging.LogRecord("test_chunk_overflow_truncate_fail", logging.INFO,
-                          None, None, "1" * 1000, None, None))
+        logging.LogRecord(
+            "test_chunk_overflow_truncate_fail",
+            logging.INFO,
+            None,
+            None,
+            "1" * 1000,
+            None,
+            None,
+        )
+    )
     with pytest.warns(GELFTruncationFailureWarning):
         list(GELFTruncatingChunker(1).chunk_message(message))
 
 
 def test_chunk_overflow_truncate_fail_large_inherited_field():
-    message = BaseGELFHandler(facility="this is a really long facility" * 5000)\
-        .makePickle(logging.LogRecord("test_chunk_overflow_truncate_fail", logging.INFO, None, None, "reasonable message", None, None))
+    message = BaseGELFHandler(
+        facility="this is a really long facility" * 5000
+    ).makePickle(
+        logging.LogRecord(
+            "test_chunk_overflow_truncate_fail",
+            logging.INFO,
+            None,
+            None,
+            "reasonable message",
+            None,
+            None,
+        )
+    )
     with pytest.warns(GELFTruncationFailureWarning):
         list(GELFTruncatingChunker(2).chunk_message(message))
